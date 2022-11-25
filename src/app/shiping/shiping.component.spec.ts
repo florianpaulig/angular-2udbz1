d2083a932c6bb9763567c78acd391f48fdb7afe3
florianpaulig <109411307+florianpaulig@users.noreply.github.com>
2022-11-25 15:34:22 +0100

    4 files modified, 10 added

---
+++ src/app/shiping/shiping.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ShipingComponent } from './shiping.component';
+
+describe('ShipingComponent', () => {
+  let component: ShipingComponent;
+  let fixture: ComponentFixture<ShipingComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ShipingComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ShipingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shiping/shiping.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shiping',
+  templateUrl: './shiping.component.html',
+  styleUrls: ['./shiping.component.css']
+})
+export class ShipingComponent {
+
+}
